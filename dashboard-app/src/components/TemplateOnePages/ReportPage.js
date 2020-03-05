3d9df949b9a003e25e7fcd5105010dccddaae2a1
Brady <brady.christensen@helixcp.com>
2020-03-04 16:28:58 -0800

    basic data handling

--- dashboard-app/src/components/TemplateOnePages/ReportPage.js
+++ dashboard-app/src/components/TemplateOnePages/ReportPage.js
@@ -16,20 +16,39 @@ const InformationText = function() {
 
 const SummaryTable = (props) => {
 
+    // temporary, will replace with json handled through props
+    var dataObjects = [
+        {name: "JonDo", data: [["192.168.4.14", 9224]]},
+        {name: "Proxy.HTTP", data: [["192.168.5.1", 26], ["192.168.5.2", 9], ["192.168.5.3", 2]]}
+    ];
+
     const renderTable = () => {
         var data = new window.google.visualization.DataTable();
+        // TODO replace column generation with columns given by data fields
         data.addColumn('string', 'Application');
         data.addColumn('string', 'User (or IP)');
         data.addColumn('number', 'Session');
         data.addColumn('number', '% of Subtotal');
         
-        data.addRows([
-            ['JonDo', '192.168.4.14', 9224, 100.00],
-            ['Proxy.HTTP', '222.186.19.221', 146, 37.15]
-        ]);
+        for (var dataObject of dataObjects) {
+            var objectData = dataObject.data;
+            var objectValues = objectData.map((cur) => cur[1]);
+
+            var total = objectValues.reduce((prev, cur) => prev + cur);
+            var percentages = objectValues.map((cur) => (cur / total) * 100);
+
+            var firstRow = objectData[0];
+            data.addRow([dataObject.name, firstRow[0], firstRow[1], percentages[0]]);
+            for (var i = 1; i < objectData.length; i++) {
+                data.addRow(['', objectData[i][0], objectData[i][1], percentages[i]])
+            }
+        }
+
+        var percentFormat = new window.google.visualization.NumberFormat({suffix: '%'});
+        percentFormat.format(data, 3);
 
         var options = {
-
+            sort: 'disable'
         };
 
         var table = new window.google.visualization.Table(document.getElementById(props.tableName));
