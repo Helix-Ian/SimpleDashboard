90c4aad5a187f94d3c78dd07bc2c99dfe003991f
Helix-Ian <61526612+Helix-Ian@users.noreply.github.com>
2020-03-08 19:41:16 -0700

    Update UserDashboard.js

--- dashboard-app/src/components/UserDashboard.js
+++ dashboard-app/src/components/UserDashboard.js
@@ -9,7 +9,6 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import PersistentDrawerLeft from './TemplateOnePages/ToCDrawer'
 
 
 const UserDashboard = (props) => {
