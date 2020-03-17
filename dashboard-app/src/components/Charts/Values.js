d1b36065b816f79bf3231299d2021650e51c850a
Brady <brady.christensen@helixcp.com>
2020-03-17 11:42:22 -0700

    comment method

--- dashboard-app/src/components/Charts/Values.js
+++ dashboard-app/src/components/Charts/Values.js
@@ -4,6 +4,11 @@ const Values = (props) => {
     
     var content = props.info.Content;
 
+    /**
+     * Decodes an html-escaped string into its raw form
+     * @param {string} str the escaped string to decode
+     * @returns {string} the decoded html
+     */
     function decodeHTML(str) {
         var txt = document.createElement('textarea');
         txt.innerHTML = str;
